44e503badf7eb8daf4c2d27db968297a5a33ba38
nicolas velardez <nicogor00@gmail.com>
2022-01-18 21:29:41 -0300

    segunda entrega

---
+++ primer/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+export default  function NavBar (){
+    return(
+
+        <>
+            <div>
+                <ul>
+                    <li>INICIO</li>
+                    <li>QUIENES SOMOS</li>
+                    <li>SERVICIOS</li>
+                    <li>CONTACTO</li>
+                 </ul>
+            </div>
+        </>
+
+    )
+
+}
